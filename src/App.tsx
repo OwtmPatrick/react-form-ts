0657fe9a5ace88b6703276164a5bce9aeb2ee995
Radik Zangirov <owtmpatrick@gmail.com>
2023-01-29 16:34:31 +0500

    change default value

--- src/App.tsx
+++ src/App.tsx
@@ -11,132 +11,85 @@ import { Field } from './types';
 const defaultValue = {
   type: 'object',
   properties: {
-    firstName: {
+    streetAddress: {
+      type: 'string',
+    },
+    city: {
+      type: 'string',
+    },
+    state: {
       type: 'string',
     },
     gender: {
-      enum: ['male', 'female'],
+      enum: ['male', 'female', 'else'],
     },
-    boolean: {
-      type: 'boolean',
-    },
-    age: {
-      type: 'integer',
+    phones: {
+      type: 'array',
+      maxItems: 3,
+      minItems: 1,
+      items: {
+        type: 'string',
+        minLength: 1,
+        maxLength: 255,
+      },
     },
     parents: {
       type: 'array',
+      minItems: 0,
+      maxItems: 2,
       items: {
         type: 'object',
         properties: {
-          name: {
-            type: 'string',
+          relation: {
+            enum: ['mother', 'father'],
           },
           age: {
             type: 'integer',
           },
-          grandparents: {
-            type: 'array',
-            items: {
-              type: 'object',
-              properties: {
-                name: {
-                  type: 'string',
-                },
-                retired: {
-                  type: 'boolean',
-                },
+          name: {
+            type: 'string',
+          },
+          secondName: {
+            type: 'string',
+          },
+          grandFather: {
+            type: 'object',
+            name: {
+              type: 'string',
+            },
+            age: {
+              type: 'integer',
+            },
+            secondName: {
+              type: 'string',
+            },
+            retired: {
+              type: 'boolean',
+            },
+          },
+          grandMother: {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+              },
+              age: {
+                type: 'integer',
+              },
+              secondName: {
+                type: 'string',
+              },
+              retired: {
+                type: 'boolean',
               },
             },
           },
         },
       },
-      minItems: 0,
-      maxItems: 2,
     },
   },
-  required: ['firstName', 'age', 'gender', 'boolean', 'parents'],
+  required: ['streetAddress', 'city', 'state'],
 };
-
-// const defaultValue = {
-//   type: 'object',
-//   properties: {
-//     streetAddress: {
-//       type: 'string',
-//     },
-//     city: {
-//       type: 'string',
-//     },
-//     state: {
-//       type: 'string',
-//     },
-//     gender: {
-//       enum: ['male', 'female', 'else'],
-//     },
-//     phones: {
-//       type: 'array',
-//       maxItems: 3,
-//       minItems: 1,
-//       items: {
-//         type: 'string',
-//         minLength: 1,
-//         maxLength: 255,
-//       },
-//     },
-//     parents: {
-//       type: 'array',
-//       minItems: 0,
-//       maxItems: 2,
-//       items: {
-//         type: 'object',
-//         properties: {
-//           relation: {
-//             enum: ['mother', 'father'],
-//           },
-//           age: {
-//             type: 'integer',
-//           },
-//           name: {
-//             type: 'string',
-//           },
-//           secondName: {
-//             type: 'string',
-//           },
-//           grandFather: {
-//             type: 'object',
-//             name: {
-//               type: 'string',
-//             },
-//             age: {
-//               type: 'integer',
-//             },
-//             secondName: {
-//               type: 'string',
-//             },
-//             retired: {
-//               type: 'boolean',
-//             },
-//           },
-//           //   grandMother: {
-//           //     type: 'object',
-//           //     name: {
-//           //       type: 'string',
-//           //     },
-//           //     age: {
-//           //       type: 'integer',
-//           //     },
-//           //     secondName: {
-//           //       type: 'string',
-//           //     },
-//           //     retired: {
-//           //       type: 'boolean',
-//           //     },
-//           //   },
-//         },
-//       },
-//     },
-//   },
-//   required: ['streetAddress', 'city', 'state'],
-// };
 
 const App = () => {
   const [schema, setSchema] = useState(defaultValue as Field);
@@ -144,7 +97,12 @@ const App = () => {
 
   return (
     <>
-      <Stack p={largeScreen ? 2 : 1} spacing={5} direction={largeScreen ? 'row' : 'column'}>
+      <Stack
+        p={largeScreen ? 2 : 1}
+        spacing={5}
+        direction={largeScreen ? 'row' : 'column'}
+        height="100%"
+      >
         <SchemaInput setSchema={setSchema} defaultValue={defaultValue} />
 
         <Form schema={schema} />
